cd080db90f8d9e25d4111c92a3ff36e80d480b26
Sanni Moshood <sanniabiolamoshood95@gmail.com>
2020-10-31 11:21:03 +0100

    packages updated

--- models/User.js
+++ models/User.js
@@ -2,8 +2,37 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+    first_name : {
+        type : String,
+        required : true
+    },
+    last_name : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    phone : {
+        type : String,
+        required :true
+    },
+    password : {
+        type : String,
+        required : true
+    }
+    ,
+    user_type : {
+        type : String,
+        enum : ["user","admin"],
+        default : "user"
+    }
+},
+{ timestamps: true });
 
 
-})
+//hash user password before save
+
 
 module.exports = User = mongoose.model("users", UserSchema);
